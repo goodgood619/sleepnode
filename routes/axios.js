c78cfedcb7e58a75287c07a260ed381dcaa00df1
이윤석 <dream9369yy@gmail.com>
2020-07-02 03:41:24 +0900

    axios -> puppeteer로 수정...(axios가 crawling을 제대로 못해서.. ㅠ)

--- routes/axios.js
+++ routes/axios.js
@@ -1,37 +1,68 @@
 const express = require('express')
 const router = express.Router()
+const puppeteer = require('puppeteer')
 const axios = require('axios')
 const cheerio = require('cheerio')
 const youtubeThumbnail = require('youtube-thumbnail')
 router.post('/youtube',async (req,res)=>{
     let url = req.body.link[0]
-    const getHtml = async () => {
-        try {
-            return await axios.get(url);
-        }catch (e) {
-            console.log(e)
-        }
+    // const getHtml = async () => {
+    //     try {
+    //         return await axios.get(url);
+    //     }catch (e) {
+    //         console.log(e)
+    //     }
+    // };
+    const async1 =  ()=>{
+        return new Promise((fulfilled,rejected)=>{
+            puppeteer.launch({headless: true}).then(async browser => {
+                const page = await browser.newPage();
+                await page.goto(url);
+                try {
+                    await page.waitForSelector('h1');
+                    await page.waitForSelector('ytd-video-owner-renderer div ytd-channel-name div div yt-formatted-string a');
+                    const title = await page.evaluate(()=> document.querySelector('h1').textContent);
+                    const who = await page.evaluate(()=>document.querySelector('ytd-video-owner-renderer div ytd-channel-name div div yt-formatted-string a').textContent);
+                    // console.log(title)
+                    // console.log(who)
+                    fulfilled({title,who})
+                }
+                catch(err){
+                    // console.log(err)
+                    rejected(err)
+                }
+            })
+        })
     };
-    const one = getHtml()
-        .then(html=> {
-            const $ = cheerio.load(html.data);
-            let linkTitle="", linkChannel = "";
-            $('h1 > span').each(function (i,e) {
-                linkTitle = $(e).text().slice(5,$(e).text().length-3);
-            })
-            $('div > div > div > div > div > div > div > div > div > div > div > a').each(function (i,e) {
-                linkChannel = $(e).text()
-            })
-            return {linkTitle,linkChannel}
-        })
-        one.then(ret => {
-            const thumbnail = youtubeThumbnail(url).high.url
-            res.json({linkTitle: ret.linkTitle, linkChannel : ret.linkChannel,linkAddress : url,
+    async1().then(({title,who})=>{
+        const thumbnail = youtubeThumbnail(url).high.url
+        res.json({linkTitle: title, linkChannel : who,linkAddress : url,
             thumbnailUrl : thumbnail})
-        })
-            .catch(err=> {
-                console.log(err)
-            })
+    }).catch((error)=>{
+        console.log(error)
+        res.json({err :error})
+    })
+    // const one = getHtml()
+    //     .then(html=> {
+    //         const $ = cheerio.load(html.data);
+    //         let linkTitle="", linkChannel = "";
+    //         $('h1 > span').each(function (i,e) {
+    //             linkTitle = $(e).text().slice(5,$(e).text().length-3);
+    //         })
+    //         $('div > div > div > div > div > div > div > div > div > div > div > a').each(function (i,e) {
+    //             linkChannel = $(e).text()
+    //         })
+    //         return {linkTitle,linkChannel}
+    //     })
+    //     one.then(ret => {
+    //         const thumbnail = youtubeThumbnail(url).high.url
+    //         console.log(ret)
+    //         res.json({linkTitle: ret.linkTitle, linkChannel : ret.linkChannel,linkAddress : url,
+    //         thumbnailUrl : thumbnail})
+    //     })
+    //         .catch(err=> {
+    //             console.log(err)
+    //         })
 })
 
 router.post('/twitch',async (req,res)=>{
